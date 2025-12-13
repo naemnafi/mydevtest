e13b9a6e0d40731ab8749e1fe42a5f4ba99410c5
Naem Nafi <naemmuttaqi@gmail.com>
2025-12-14 00:42:39 +0600

    include files

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert("hi js");
+
+let sum = 50 + 40;
+console.log(sum);
+
+if (sum > 100) {
+console.log('Expensive');
+} else {
+    console.log('Cheap');
+}
+document.getElementById('title').innerHTML = ('hellow js');
