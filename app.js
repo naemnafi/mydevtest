8d4adf919328c2d499672901fd2e19d93929be8e
Naem Nafi <naemmuttaqi@gmail.com>
2025-12-16 13:47:50 +0600

    update 16/12

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ console.log('Expensive');
     console.log('Cheap');
 }
 document.getElementById('title').innerHTML = ('hellow js');
+let firstName = 'naem';
+console.log(firstName);
+
+let fruits = 'apple';
+console.log(`${fruits} is my fav`);
+
+var z = 100;
+console.log(z);
+
+// Arithmatic operator
+let a = 20;
+let b = 10;
+let c = a + b;
+console.log(c);
+
+// Prompt, userInput, modulus,
+let userInput = prompt('Enter a number');
+let number = parseInt(userInput);
+if (number % 2 === 0){
+    console.log('Even');
+} else {
+    console.log ('Odd');
+}
